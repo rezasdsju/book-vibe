cf582f103950de367f4684d144ad36521934b98e
rezasdsju <rezaul2002104@gmail.com>
2026-09-21 22:17:55 +0600

    chore: add loading fallback

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GlobalLoading = () => {
+    return (
+        <div className='flex items-center justify-center py-20'>
+            <span className="loading loading-spinner text-secondary"></span>
+
+        </div>
+    );
+};
+
+export default GlobalLoading;
